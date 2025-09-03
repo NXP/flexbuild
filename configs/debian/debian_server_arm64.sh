3537c27a722f69091c973ac9678c28ced9f37089
nxa22691 <andy.tang@nxp.com>
2025-09-03 14:31:08 +0900

    refine the rootfs contructing script
    Signed-off-by: Andy Tang <andy.tang@nxp.com>

--- configs/debian/debian_server_arm64.sh
+++ configs/debian/debian_server_arm64.sh
@@ -60,16 +60,16 @@ mkdir -p "$ROOTDIR"/usr/local/bin \
          "$ROOTDIR"/etc/systemd/system/graphical.target.wants \
          "$ROOTDIR"/etc/systemd/system/local-fs.target.wants
 
-cp -f src/system/boot.mount            "$ROOTDIR"/lib/systemd/system/
-cp -f tools/flex-installer            "$ROOTDIR"/usr/bin/
-cp -f tools/resizerfs                  "$ROOTDIR"/usr/bin/
-cp -f src/system/udev/udev-rules-*/*.rules "$ROOTDIR"/etc/udev/rules.d/
-cp -f src/system/distroplatcfg         "$ROOTDIR"/usr/bin/
-cp -f src/system/platcfg.service       "$ROOTDIR"/lib/systemd/system/
-cp -f src/system/blacklist.conf        "$ROOTDIR"/etc/modprobe.d/
-cp -f src/system/ts.conf               "$ROOTDIR"/etc/ts.conf.bak
-cp -f src/system/board_id.sh           "$ROOTDIR"/usr/bin/
-cp -f src/system/80-wired.network      "$ROOTDIR"/usr/lib/systemd/network/
+install -D -m 644 src/system/boot.mount            "$ROOTDIR"/lib/systemd/system/boot.mount
+install -D -m 755 tools/flex-installer             "$ROOTDIR"/usr/bin/flex-installer
+install -D -m 755 tools/resizerfs                  "$ROOTDIR"/usr/bin/resizerfs
+install -D -m 644 src/system/udev/udev-rules-*/*.rules "$ROOTDIR"/etc/udev/rules.d/
+install -D -m 755 src/system/distroplatcfg         "$ROOTDIR"/usr/bin/distroplatcfg
+install -D -m 644 src/system/platcfg.service       "$ROOTDIR"/lib/systemd/system/platcfg.service
+install -D -m 644 src/system/blacklist.conf        "$ROOTDIR"/etc/modprobe.d/blacklist.conf
+install -D -m 644 src/system/ts.conf               "$ROOTDIR"/etc/ts.conf.bak
+install -D -m 755 src/system/board_id.sh           "$ROOTDIR"/usr/bin/board_id.sh
+install -D -m 644 src/system/80-wired.network      "$ROOTDIR"/usr/lib/systemd/network/80-wired.network
 
 # ---- Rootfs configuration inside chroot ----
 #echo "[POST_ROOTFS] Configuring rootfs in chroot"
@@ -84,15 +84,15 @@ mkdir -p /usr/local/bin \
          /etc/systemd/system/local-fs.target.wants
 
 # User and group setup
-useradd -m -d /home/debian -s /bin/bash debian >/dev/null || true
-groupadd wayland >/dev/null || true
+id -u debian &>/dev/null || useradd -m -d /home/debian -s /bin/bash debian
+getent group wayland &>/dev/null || groupadd wayland
 usermod -aG sudo,input,video,wayland,render debian || true
 passwd --delete root >/dev/null || true
 passwd --delete debian >/dev/null || true
 
 # SSH configuration
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
+grep -q '^PermitRootLogin' /etc/ssh/sshd_config || echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+grep -q '^PermitEmptyPasswords' /etc/ssh/sshd_config || echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
 
 # systemd service symlinks
 ln -sf /lib/systemd/system/platcfg.service /etc/systemd/system/multi-user.target.wants/platcfg.service
@@ -107,38 +107,48 @@ ln -sf /boot/firmware /lib/firmware
 
 # Locale setup
 sed -i -e "s/.*en_US.UTF-8.*/en_US.UTF-8 UTF-8/" /etc/locale.gen
-/usr/sbin/locale-gen >/dev/null
+/usr/sbin/locale-gen >/dev/null 2>&1 || true
 echo "localhost" > /etc/hostname
-/usr/sbin/update-locale LANG=en_US.UTF-8
+/usr/sbin/update-locale LANG=en_US.UTF-8 || true
 
 # Root user bashrc
-printf "COGL_DRIVER=gles2\nCLUTTER_DRIVER=gles2\nQT_QPA_PLATFORM=wayland\nGDK_GL=gles\n" >> /etc/environment
-printf "QT_QUICK_BACKEND=software\n" >> /etc/environment
-printf "\nexport DISPLAY=:0\nexport WAYLAND_DISPLAY=wayland-0" >> /root/.bashrc
+cat >> /etc/environment <<'EOT'
+COGL_DRIVER=gles2
+CLUTTER_DRIVER=gles2
+QT_QPA_PLATFORM=wayland
+GDK_GL=gles
+QT_QUICK_BACKEND=software
+EOT
+
+grep -q 'WAYLAND_DISPLAY' /root/.bashrc || \
+	printf "\nexport DISPLAY=:0\nexport WAYLAND_DISPLAY=wayland-0\n" >> /root/.bashrc
 
 # Other system settings
 printf "/usr/lib\n" >> /etc/ld.so.conf.d/01-sdk.conf
-printf " * Support:   https://www.nxp.com/support\n" >> /etc/update-motd.d/20-help-text
-printf " * Licensing: https://lsdk.github.io/eula\n" >> /etc/update-motd.d/20-help-text
+{
+  echo " * Support:   https://www.nxp.com/support"
+  echo " * Licensing: https://lsdk.github.io/eula"
+} > /etc/update-motd.d/20-help-text
 printf "Build: $(date --rfc-3339 seconds)\n" >> /etc/buildinfo
 EOF
 
-printf "$DISTRIB_NAME $DISTRIB_VERSION Debian $DEBIAN_VERSION (optimized with NXP-specific hardware acceleration)\n" >> $ROOTDIR/etc/issue
+printf "%s %s (optimized with NXP-specific hardware acceleration)\n" \
+  "${DISTRIB_NAME:-Debian}" "${DISTRIB_VERSION:-lsdk2512}" >> "$ROOTDIR"/etc/issue
 # ---- Board-specific customization on host ----
 # echo "[POST_ROOTFS] Board-specific configuration"
 
 case "$MACHINE" in
     ls1028*)
-        mkdir -p "$ROOTDIR"/etc/xdg/weston
-        cp -f "$FBDIR"/src/system/weston/weston.ini.ls1028 "$ROOTDIR"/etc/xdg/weston/
+		install -D -m 644 "$FBDIR"/src/system/weston/weston.ini.ls1028 \
+			"$ROOTDIR"/etc/xdg/weston/weston.ini
         ;;
     imx*)
         mkdir -p "$ROOTDIR"/etc/xdg/weston
         cp -f "$FBDIR"/src/system/weston/weston.ini* "$ROOTDIR"/etc/xdg/weston/
         if [ -f "$ROOTDIR"/etc/pam.d/gdm-password ]; then
-            cp -f "$FBDIR"/src/system/gdm/daemon.conf "$ROOTDIR"/etc/gdm3
-            cp -f "$FBDIR"/src/system/gdm/gdm-password "$ROOTDIR"/etc/pam.d
-            cp -f "$FBDIR"/src/system/gdm/gdm.service "$ROOTDIR"/lib/systemd/system/
+            install -D -m 644 "$FBDIR"/src/system/gdm/daemon.conf "$ROOTDIR"/etc/gdm3
+            install -D -m 644 "$FBDIR"/src/system/gdm/gdm-password "$ROOTDIR"/etc/pam.d
+            install -D -m 644 "$FBDIR"/src/system/gdm/gdm.service "$ROOTDIR"/lib/systemd/system/
             ln -sf /lib/systemd/system/gdm.service "$ROOTDIR"/etc/systemd/system/graphical.target.wants/gdm.service
         fi
         ;;

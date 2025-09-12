5d8775f438aeaface24d1c5c911a56e09d64204d
Yuantian Tang <andy.tang@nxp.com>
2025-09-11 23:13:21 -0700

    move imx_firmware and firmware-imx to rootfs
    Signed-off-by: Andy Tang <andy.tang@nxp.com>

---
+++ src/apps/utils/firmware_imx.mk
@@ -0,0 +1,23 @@
+# Copyright 2017-2025 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+
+firmware_imx:
+	@[ $(SOCFAMILY) != IMX ] && exit || \
+	$(call dl_by_wget,firmware_imx_bin,firmware_imx.bin) && \
+	echo Installing firmware-imx for ddr,hdmi,dp,vpu,easrc,epdc,xcvr,xuvi && \
+	cd $(UTILSDIR) && \
+	if [ ! -d "$(UTILSDIR)"/firmware_imx ]; then \
+		chmod +x $(FBDIR)/dl/firmware_imx.bin; \
+		$(FBDIR)/dl/firmware_imx.bin --auto-accept --force $(LOG_MUTE); \
+		mv firmware-imx-* firmware_imx; \
+	fi && \
+	cd $(UTILSDIR)/firmware_imx && \
+	mkdir -p $(DESTDIR)/lib/firmware/imx && \
+	rsync -a ./firmware/ $(DESTDIR)/lib/firmware/imx/  --exclude=ddr && \
+	if [[ $(MACHINE) == imx95* ]]; then \
+		cp ./firmware/vpu/wave633c_codec_fw.bin $(DESTDIR)/lib/firmware; \
+	fi && \
+	$(call fbprint_d,"Installing [Done]")

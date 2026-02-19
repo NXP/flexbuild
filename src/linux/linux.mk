cf88e22aa7c3b277095befa621611deaefe1cb85
Yuantian Tang <andy.tang@nxp.com>
2026-02-19 07:33:10 -0800

    Optimize the linux kernel build script
    Remove the unnecessary trailing \ and &&.
    Also remove arm32 related code and use more makefile feature.
    
    Signed-off-by: Andy Tang <andy.tang@nxp.com>

--- src/linux/linux.mk
+++ src/linux/linux.mk
@@ -1,85 +1,69 @@
 #
-# Copyright 2017-2023 NXP
+# Copyright 2017-2023,2026 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
 
+KOUTDIR := $(KERNEL_OUTPUT_PATH)/$(KERNEL_BRANCH)
+KTGT_DIR := $(FBOUTDIR)/linux/$(KERNEL_TREE)/arm64/$(SOCFAMILY)
+DTBSTR := $(if $(filter y,$(CONFIG_PLATFORM_IMX)),imx*.dtb,fsl*.dtb)
+KERNEL_IMAGE := $(KTGT_DIR)/Image
 
-linux:
-	$(call download_repo,linux,linux) && \
-	$(call patch_apply,linux,linux) && \
-	cd $(KERNEL_PATH) && \
-	$(call fbprint_b,"$(KERNEL_TREE) with $(KERNEL_BRANCH)") && \
-	$(call fbprint_n,"Compiler = `$(CROSS_COMPILE)gcc --version | head -1`") && \
-	if [ $(DESTARCH) = arm64 -a $(SOCFAMILY) = IMX ]; then \
-	    locarch=arm64; dtbstr=freescale/imx*.dtb; \
-	elif [ $(DESTARCH) = arm64 -a $(SOCFAMILY) = LS ]; then \
-	    locarch=arm64; dtbstr=freescale/fsl*.dtb; extflags="DTC_FLAGS='-@'"; \
-	elif [ $(DESTARCH) = arm32 -a $(SOCFAMILY) = LS ]; then \
-	    locarch=arm; dtbstr=ls*.dtb; \
-	elif [ $(DESTARCH) = arm32 -a $(SOCFAMILY) = IMX ]; then \
-	    locarch=arm; dtbstr=imx*.dtb; \
-	fi && \
-	opdir=$(KERNEL_OUTPUT_PATH)/$(KERNEL_BRANCH) && mkdir -p $$opdir/tmp && \
-	if [ "$(BUILDARG)" = "custom" ]; then \
-	    $(MAKE) menuconfig -C $(KERNEL_PATH) O=$$opdir && \
-	    $(call fbprint_d,"Custom kernel config: $$opdir/.config") && \
-	    $(call fbprint_n,"Run 'bld linux' to proceed with the customized .config above") && exit; \
-	fi; \
-	$(call fbprint_n,"Total Config List = $(KERNEL_CFG) $(FRAGMENT_CFG)") && \
-	if [ ! -f $$opdir/.config ]; then \
-	    $(MAKE) $(KERNEL_CFG) $(FRAGMENT_CFG) -C $(KERNEL_PATH) O=$$opdir 1>/dev/null 2>&1; \
-	fi && \
-	if [ "$(ENDIANTYPE)" = "be" ]; then \
-	    sed -i 's/# CONFIG_CPU_BIG_ENDIAN is not set/CONFIG_CPU_BIG_ENDIAN=y/' $$opdir/.config; \
-	    echo Big-Endian enabled!; \
-	fi && \
-	$(MAKE) -j$(JOBS) all -C $(KERNEL_PATH) O=$$opdir $(LOG_MUTE) && \
-	if [ $(DESTARCH) = arm32 ]; then \
-	    $(MAKE) -j$(JOBS) uImage LOADADDR=80008000 -C $(KERNEL_PATH) O=$$opdir; \
-	fi && \
-	if [ $(DESTARCH) = arm32 -o $(DESTARCH) = arm64 ]; then \
-	    $(MAKE) zinstall \
-	    INSTALL_PATH=$(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY) -C $(KERNEL_PATH) O=$$opdir; \
-	fi && \
-	if [ $(DESTARCH) = arm64 ]; then \
-	    cp $$opdir/arch/$$locarch/boot/Image* $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY); \
-	elif [ $(DESTARCH) = arm32 ]; then \
-	    cp -f $$opdir/arch/$$locarch/boot/uImage $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY); \
-	    cp -f $$opdir/arch/$$locarch/boot/zImage $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY); \
-	fi && \
-	$(MAKE) -j$(JOBS) modules -C $(KERNEL_PATH) O=$$opdir $(LOG_MUTE) && \
-	$(MAKE) -j$(JOBS)  modules_install INSTALL_MOD_PATH=$$opdir/tmp -C $(KERNEL_PATH) O=$$opdir $(LOG_MUTE) && \
-	ls $$opdir/arch/$$locarch/boot/dts/$$dtbstr | xargs -I {} cp {} $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY) && \
-	ls -l $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY) $(LOG_MUTE) && \
-	$(call fbprint_d,"$(KERNEL_TREE) $(KERNEL_BRANCH) in $(FBOUTDIR)/linux/$(KERNEL_TREE)/$(DESTARCH)/$(SOCFAMILY)")
+define kernel_pre
+	@$(call download_repo,linux,linux)
+	$(call patch_apply,linux,linux)
+	$(call fbprint_b,"$(KERNEL_TREE) with $(KERNEL_BRANCH)")
+	mkdir -p $(KOUTDIR)/tmp
+	if [ -f "$(FBDIR)/configs/linux/$(CONFIG_LINUX_EXTRA_CONFIG)" ]; then \
+		cp -f "$(FBDIR)/configs/linux/$(CONFIG_LINUX_EXTRA_CONFIG)" $(KERNEL_PATH)/arch/arm64/configs/
+	fi
+	$(call fbprint_n,"Total Config List = $(KERNEL_CFG) $(CONFIG_LINUX_EXTRA_CONFIG)")
+	if [ ! -f $(KOUTDIR)/.config ]; then \
+	    $(MAKE) $(KERNEL_CFG) $(CONFIG_LINUX_EXTRA_CONFIG) -C $(KERNEL_PATH) O=$(KOUTDIR) 1>/dev/null 2>&1
+	fi
+
+endef
+
+kernel-menuconfig linux-menuconfig:
+	@$(call kernel_pre)
+	$(MAKE) menuconfig -C $(KERNEL_PATH) O=$(KOUTDIR)
+	$(call FB_OUT,The kernel configuration is complete. Please run 'make linux' to compile the kernel)
+
+linux $(KERNEL_IMAGE):
+	@$(call kernel_pre)
+	$(MAKE) all -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
+	$(MAKE) zinstall INSTALL_PATH=$(KTGT_DIR) -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
+	cp $(KOUTDIR)/arch/arm64/boot/Image* $(KTGT_DIR)
+	\
+	$(MAKE) modules -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
+	$(MAKE) modules_install INSTALL_MOD_PATH=$(KOUTDIR)/tmp -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
+	cp $(KOUTDIR)/arch/arm64/boot/dts/freescale/$(DTBSTR) $(KTGT_DIR)
+	$(call fbprint_d,"$(KERNEL_TREE) $(KERNEL_BRANCH) in $(KTGT_DIR)")
 
 
 
-
+cryptodev_linux mdio_proxy_module isp_vvcam_module nxp_wlan_bt: $(KERNEL_IMAGE)
 linux-modules: cryptodev_linux mdio_proxy_module isp_vvcam_module nxp_wlan_bt
-	 $(call fbprint_d,"linux-modules")
+	@$(call fbprint_d,"linux-modules")
 
 
 
-linux-headers:
-	@$(call download_repo,linux,linux) && \
-	 cd $(PKGDIR)/linux && \
-	 opdir=$(KERNEL_OUTPUT_PATH)/$(KERNEL_BRANCH) && mkdir -p $$opdir/tmp && \
-	 mkdir -p $(DESTDIR)/usr && \
-	 $(MAKE) -j$(JOBS) headers_install INSTALL_HDR_PATH=$(DESTDIR)/usr -C $(KERNEL_PATH) O=$$opdir $(LOG_MUTE) && \
+linux-headers: $(KERNEL_IMAGE)
+	@$(call download_repo,linux,linux)
+	 mkdir -p $(DESTDIR)/usr
+	 $(MAKE) headers_install INSTALL_HDR_PATH=$(DESTDIR)/usr -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
 	 $(call fbprint_d,"linux-headers")
 
-
-
-#linux-deb:
+# ------------------------------
+# Build Debian kernel packages
+# ------------------------------
 linux-deb: linux
-	@$(call fbprint_b,linux debian packages) && \
-	opdir=$(KERNEL_OUTPUT_PATH)/$(KERNEL_BRANCH) && \
-	mkdebian_file=$(PKGDIR)/linux/linux/scripts/package/mkdebian && \
+	@$(call fbprint_b,linux debian packages)
+	mkdebian_file=$(PKGDIR)/linux/linux/scripts/package/mkdebian
 	if [ -f $$mkdebian_file ]; then \
 		sed -i '/^ libssl-dev:native,/ s/, libssl-dev.*/,/' $$mkdebian_file; \
-	fi && \
-	$(MAKE) -j$(JOBS) bindeb-pkg -C $(KERNEL_PATH) O=$$opdir $(LOG_MUTE) && \
-	cp -f $(KERNEL_OUTPUT_PATH)/*.deb $(FBOUTDIR)/images/ && \
+	fi
+	$(MAKE) bindeb-pkg -C $(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
+	mkdir -p $(FBOUTDIR)/images
+	cp -f $(KERNEL_OUTPUT_PATH)/*.deb $(FBOUTDIR)/images/
 	$(call fbprint_d,"linux-deb in: $(FBOUTDIR)/images")

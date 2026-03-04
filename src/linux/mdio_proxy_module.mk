94fc42ecffc02df6c97d848067447d16080aef9c
Chunguang Li <chunguang.li_2@nxp.com>
2026-03-03 22:41:13 -0800

    Improve portability by avoiding make -C
    Switch to the standard (cd dir && $(MAKE)) pattern, which is more
    POSIX-compliant and ensures the build process works
    across a wider range of platforms.
    
    Signed-off-by: Chunguang Li <chunguang.li_2@nxp.com>

--- src/linux/mdio_proxy_module.mk
+++ src/linux/mdio_proxy_module.mk
@@ -8,7 +8,7 @@ mdio_proxy_module:
 	@$(call download_repo,mdio_proxy_module,linux)
 	$(call patch_apply,mdio_proxy_module,linux)
 	$(call fbprint_b,"mdio-proxy-module")
-	$(MAKE) KBUILD_DIR=$(KERNEL_PATH) O=$(KOUTDIR) \
-		-C $(PKGDIR)/linux/mdio_proxy_module $(LOG_MUTE)
+	cd $(PKGDIR)/linux/mdio_proxy_module
+	$(MAKE) KBUILD_DIR=$(KERNEL_PATH) O=$(KOUTDIR) $(LOG_MUTE)
 	cp -f mdio-proxy.ko $(KOUTDIR)/tmp/lib/modules/*/kernel/drivers/net/mdio/
 	$(call fbprint_d,"mdio_proxy_module")
